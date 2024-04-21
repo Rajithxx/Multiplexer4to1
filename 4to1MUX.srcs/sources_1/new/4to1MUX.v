1d13ddd74a1fb8c2ebcc9d9e9786c64058879f54
Rajithxx <148357145+Rajithxx@users.noreply.github.com>
2024-04-21 15:10:30 +0530

    Update 4to1MUX.v

--- 4to1MUX.srcs/sources_1/new/4to1MUX.v
+++ 4to1MUX.srcs/sources_1/new/4to1MUX.v
@@ -1,26 +1,10 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.01.2024 11:39:48
-// Design Name: 
-// Module Name: 4to1MUX
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module m41 ( input a, 
+input b, 
+input c, 
+input d, 
+input s0, s1,
+output out); 
 
-module Fourto1MUX(y, a, b, c, d, s0, s1);
-output y;
-input a, b, c, d, s0, s1;
-                               //Write the logic for 4 to 1 MUX using conditional operator                            
+assign out = s1 ? (s0 ? d : c) : (s0 ? b : a); 
+
 endmodule
